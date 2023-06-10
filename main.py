ba92f27f6bf95e4591885b482ab4b7dd6c756d2e
Joshua Hall <unstableplazma@gmail.com>
2023-06-10 12:02:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,43 +24,43 @@ pin-27: 3rd bottom left"""
 
 class display:
     def __init__(self):
-        self.pins.setup(2, pins.OUT)
-        self.pins.setup(3, pins.OUT)
-        self.pins.setup(4, pins.OUT)
-        self.pins.setup(5, pins.OUT)
-        self.pins.setup(6, pins.OUT)
-        self.pins.setup(9, pins.OUT)
-        self.pins.setup(10, pins.OUT)
-        self.pins.setup(11, pins.OUT)
-        self.pins.setup(13, pins.OUT)
-        self.pins.setup(14, pins.OUT)
-        self.pins.setup(16, pins.OUT)
-        self.pins.setup(17, pins.OUT)
-        self.pins.setup(19, pins.OUT)
-        self.pins.setup(20, pins.OUT)
-        self.pins.setup(21, pins.OUT)
-        self.pins.setup(22, pins.OUT)
-        self.pins.setup(26, pins.OUT)
-        self.pins.setup(27, pins.OUT)
+        pins.setup(2, pins.OUT)
+        pins.setup(3, pins.OUT)
+        pins.setup(4, pins.OUT)
+        pins.setup(5, pins.OUT)
+        pins.setup(6, pins.OUT)
+        pins.setup(9, pins.OUT)
+        pins.setup(10, pins.OUT)
+        pins.setup(11, pins.OUT)
+        pins.setup(13, pins.OUT)
+        pins.setup(14, pins.OUT)
+        pins.setup(16, pins.OUT)
+        pins.setup(17, pins.OUT)
+        pins.setup(19, pins.OUT)
+        pins.setup(20, pins.OUT)
+        pins.setup(21, pins.OUT)
+        pins.setup(22, pins.OUT)
+        pins.setup(26, pins.OUT)
+        pins.setup(27, pins.OUT)
 
-        self.pins.output(2, pins.LOW)
-        self.pins.output(3, pins.LOW)
-        self.pins.output(4, pins.LOW)
-        self.pins.output(5, pins.LOW)
-        self.pins.output(6, pins.LOW)
-        self.pins.output(9, pins.LOW)
-        self.pins.output(10, pins.LOW)
-        self.pins.output(11, pins.LOW)
-        self.pins.output(13, pins.LOW)
-        self.pins.output(14, pins.LOW)
-        self.pins.output(16, pins.LOW)
-        self.pins.output(17, pins.LOW)
-        self.pins.output(19, pins.LOW)
-        self.pins.output(20, pins.LOW)
-        self.pins.output(21, pins.LOW)
-        self.pins.output(22, pins.LOW)
-        self.pins.output(26, pins.LOW)
-        self.pins.output(27, pins.LOW)
+        pins.output(2, pins.LOW)
+        pins.output(3, pins.LOW)
+        pins.output(4, pins.LOW)
+        pins.output(5, pins.LOW)
+        pins.output(6, pins.LOW)
+        pins.output(9, pins.LOW)
+        pins.output(10, pins.LOW)
+        pins.output(11, pins.LOW)
+        pins.output(13, pins.LOW)
+        pins.output(14, pins.LOW)
+        pins.output(16, pins.LOW)
+        pins.output(17, pins.LOW)
+        pins.output(19, pins.LOW)
+        pins.output(20, pins.LOW)
+        pins.output(21, pins.LOW)
+        pins.output(22, pins.LOW)
+        pins.output(26, pins.LOW)
+        pins.output(27, pins.LOW)
 
         self.table = {
             0: [2, 3, 4, 5, 6, 14, 19, 22],
@@ -77,44 +77,54 @@ class display:
 
     def display(self, number):
         if number == "clear":
-            self.pins.output(2, pins.LOW)
-            self.pins.output(3, pins.LOW)
-            self.pins.output(4, pins.LOW)
-            self.pins.output(5, pins.LOW)
-            self.pins.output(6, pins.LOW)
-            self.pins.output(9, pins.LOW)
-            self.pins.output(10, pins.LOW)
-            self.pins.output(11, pins.LOW)
-            self.pins.output(13, pins.LOW)
-            self.pins.output(14, pins.LOW)
-            self.pins.output(16, pins.LOW)
-            self.pins.output(17, pins.LOW)
-            self.pins.output(19, pins.LOW)
-            self.pins.output(20, pins.LOW)
-            self.pins.output(21, pins.LOW)
-            self.pins.output(22, pins.LOW)
-            self.pins.output(26, pins.LOW)
-            self.pins.output(27, pins.LOW)
+            pins.output(2, pins.LOW)
+            pins.output(3, pins.LOW)
+            pins.output(4, pins.LOW)
+            pins.output(5, pins.LOW)
+            pins.output(6, pins.LOW)
+            pins.output(9, pins.LOW)
+            pins.output(10, pins.LOW)
+            pins.output(11, pins.LOW)
+            pins.output(13, pins.LOW)
+            pins.output(14, pins.LOW)
+            pins.output(16, pins.LOW)
+            pins.output(17, pins.LOW)
+            pins.output(19, pins.LOW)
+            pins.output(20, pins.LOW)
+            pins.output(21, pins.LOW)
+            pins.output(22, pins.LOW)
+            pins.output(26, pins.LOW)
+            pins.output(27, pins.LOW)
         elif number == 0:
-            self.table[0].forEach(self.pins.output(self.table[0], pins.HIGH))
+            for pin in self.table[0]:
+                pins.output(pin, pins.HIGH)
         elif number == 1:
-            self.table[1].forEach(self.pins.output(self.table[1], pins.HIGH))
+            for pin in self.table[1]:
+                pins.output(pin, pins.HIGH)
         elif number == 2:
-            self.table[2].forEach(self.pins.output(self.table[2], pins.HIGH))
+            for pin in self.table[2]:
+                pins.output(pin, pins.HIGH)
         elif number == 3:
-            self.table[3].forEach(self.pins.output(self.table[3], pins.HIGH))
+            for pin in self.table[3]:
+                pins.output(pin, pins.HIGH)
         elif number == 4:
-            self.table[4].forEach(self.pins.output(self.table[4], pins.HIGH))
+            for pin in self.table[4]:
+                pins.output(pin, pins.HIGH)
         elif number == 5:
-            self.table[5].forEach(self.pins.output(self.table[5], pins.HIGH))
+            for pin in self.table[5]:
+                pins.output(pin, pins.HIGH)
         elif number == 6:
-            self.table[6].forEach(self.pins.output(self.table[6], pins.HIGH))
+            for pin in self.table[6]:
+                pins.output(pin, pins.HIGH)
         elif number == 7:
-            self.table[7].forEach(self.pins.output(self.table[7], pins.HIGH))
+            for pin in self.table[7]:
+                pins.output(pin, pins.HIGH)
         elif number == 8:
-            self.table[8].forEach(self.pins.output(self.table[8], pins.HIGH))
+            for pin in self.table[8]:
+                pins.output(pin, pins.HIGH)
         elif number == 9:
-            self.table[9].forEach(self.pins.output(self.table[9], pins.HIGH))
+            for pin in self.table[9]:
+                pins.output(pin, pins.HIGH)
         else:
             print("Invalid number")
 
